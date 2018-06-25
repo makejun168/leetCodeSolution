819b895b4ca50da58aeb439fc159945a40bb8a9b
makejun168 <873800030@qq.com>
2018-06-25 10:32:43 +0800

    data

--- Simple/removeDuplicates.js
+++ Simple/removeDuplicates.js
@@ -3,15 +3,8 @@
  * @return {number}
  */
 var removeDuplicates = function (nums) {
-    if(!nums.length)return
-    let result = {};
-    for (let item of nums){
-        if (!result[item]){
-            result[item] = item;
-        }
-    }
-    return Array.from(Object.keys(result))
+    
 };
-var nums = [1,1,2];
+var nums = [1, 1, 2];
 var newNums = removeDuplicates(nums);
 console.log(newNums);
