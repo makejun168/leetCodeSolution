39dd59d671eb9cb80eaee1d1abc9e3a558adf7c5
makejun168 <873800030@qq.com>
2018-07-23 14:39:10 +0800

    '[ADD]MyHash'

---
+++ Simple/Array/MyHash.js
@@ -0,0 +1,39 @@
+/**
+ * Initialize your data structure here.
+ */
+var MyHashSet = function () {
+    this._bucket = {
+    };
+};
+
+/** 
+ * @param {number} key
+ * @return {void}
+ */
+MyHashSet.prototype.add = function (key) {
+    this._bucket.push(key);
+};
+
+/** 
+ * @param {number} key
+ * @return {void}
+ */
+MyHashSet.prototype.remove = function (key) {
+    if (!this._bucket[key]) {
+        return false;
+    }
+    this._bucket[key] = null;
+};
+
+/**
+ * Returns true if this set did not already contain the specified element 
+ * @param {number} key
+ * @return {boolean}
+ */
+MyHashSet.prototype.contains = function (key) {
+    if (this._bucket[key]) {
+        return true
+    } else {
+        return false
+    }
+};
