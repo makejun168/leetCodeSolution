5a231551a366544a16e8bcf4574fafb8805b32aa
马可骏 <makejun@Poloma.local>
2020-04-10 01:02:31 +0800

    update

--- Simple/Array/decompressRLElist.js
+++ Simple/Array/decompressRLElist.js
@@ -5,6 +5,8 @@
 var decompressRLElist = function(nums) {
     let resultArr = [];
     for (let i = 0; i < nums.length; i+=2 ) {
+        // 想要转换成数组的伪数组对象或可迭代对象。
+        // 新数组中的每个元素会执行该回调函数
         Array.from(Array(nums[i]), () => nums[i + 1])
         resultArr.push(...Array.from(Array(nums[i]), () => nums[i + 1]))
     }
