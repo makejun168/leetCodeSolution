427875613714c65451ee3aa9fcb2c96bf7fddc35
makejun168 <873800030@qq.com>
2020-04-17 18:18:26 +0800

    update

---
+++ Normal/Array/canJump.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+  let maxDistance = 0;
+    const len = nums.length;
+    for(let i = 0; i < len; i ++) {
+        if(nums[i] === 0 && maxDistance <= i) break;//如果当前位置值为0，且当前能到达的最远距离还小于等于这个位置，那么它已经走不到后面了，直接退出循环就好了
+        // 当前的 下标 + 当前的可以跳的距离 和 最大距离 比较 如果小于最大驱离 继续贪心算法
+        if(i + nums[i] > maxDistance) {
+          maxDistance = i + nums[i];
+        }
+    }
+    console.log(maxDistance >= len- 1);
+    return maxDistance >= len- 1;
+};
+
+
+canJump([2,3,1,1,4]);
