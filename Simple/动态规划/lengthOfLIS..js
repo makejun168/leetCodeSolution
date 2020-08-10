3f779dd3dde22fff90e60e2df149da5842328a41
poloma <poloma@tenvine.cn>
2020-08-10 10:07:22 +0800

    feat: 动态规划 最长递增子序列

--- Simple/动态规划/lengthOfLIS..js
+++ Simple/动态规划/lengthOfLIS..js
@@ -17,16 +17,21 @@
 function lengthOfLIS(nums) {
   let dp = new Array(nums.length).fill(1); // 默认为1
   for (let i = 0; i < nums.length; i++) {
-      for (let j = 0; j < i; j ++) {
-          if (nums[j] < nums[i]) {
-              dp[i] = Math.max(dp[i], dp[j] + 1);
-          }
+    for (let j = 0; j < i; j++) {
+      if (nums[j] < nums[i]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
       }
+    }
   }
-  
+
   let res = 0;
   for (let i = 0; i < dp.length; i++) {
-      res = Math.max(res, dp[i]);
+    res = Math.max(res, dp[i]);
   }
   return res;
 }
+
+
+// 二分查找法
+
+
