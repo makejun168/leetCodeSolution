78f05ee5d0f0c88805ce7cea028f71ad0bf9bb72
poloma <poloma@tenvine.cn>
2020-08-13 14:11:53 +0800

    feat: 0-1背包问题

---
+++ Simple/动态规划/change.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} amount
+ * @param {number[]} coins
+ * @return {number}
+ */
+
+// amount = 5, coins = [1, 2, 5]
+
+var change = function (amount, coins) {};
+
+change(5, coins);
